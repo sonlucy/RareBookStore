ab1e9993992d7f79c86c6590c5586851810d6c38
jinyucha <154579448+jinyucha@users.noreply.github.com>
2024-03-22 13:27:42 +0900

    마이페이지-로그인정보-배송지입력

---
+++ front_end/src/components/Address.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../styled/AddressView.css';
+import MypageSide from './MypageSide';
+import DestForm from './DestForm';
+import DefDestCheck from './DefDestCheck';
+
+function Address() {
+  return (
+    <div className="cyj_content_area Address-content">
+      {/* 사이드바 넣는 곳 */}
+       <div className="Address-sidebar">
+        <MypageSide />
+      </div>
+      <div className="my_Address">
+        <div className="my_Address_title">
+          <h3 className="title">배송지 입력</h3>
+        </div>
+        <div className="recent_purchase">
+          {/* 배송지 폼 컴포넌트 들어가는 곳 */}
+          <DestForm />
+        </div>
+        <div className="recent_purchase">
+         {/* 체크박스 넣는 곳 */}
+          <DefDestCheck />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Address;
