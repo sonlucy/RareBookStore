59a783b69706a548480b5ebd940105f8afc2bd65
유혜원 <yhw9212@naver.com>
2024-03-22 14:48:24 +0900

    테스트 데이터 추가

---
+++ front_end/src/testData/productInfosData.js
@@ -0,0 +1,33 @@
+// 상품 정보
+export const productInfosData = [
+  {
+    price: '12,000원',
+    seller: '아기사자',
+    grade: 3,
+    condition: ['낙서 및 필기 있음', '파손 있음', '액체로 인한 오염 있음']
+  },
+  {
+    price: '15,000원',
+    seller: '예쁜토끼',
+    grade: 4,
+    condition: []
+  },
+  {
+    price: '9,500원',
+    seller: '활기찬곰',
+    grade: 2,
+    condition: ['낙서 및 필기 있음']
+  },
+  {
+    price: '8,500원',
+    seller: '지친여우',
+    grade: 1,
+    condition: ['낙서 및 필기 있음', '액체로 인한 오염 있음']
+  },
+  {
+    price: '10,000원',
+    seller: '신난늑대',
+    grade: 3,
+    condition: ['낙서 및 필기 있음', '액체로 인한 오염 있음']
+  }
+];
