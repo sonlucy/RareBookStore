ec79ff74c854e8a71ba72d279e1d66a84feacb15
jinyucha <154579448+jinyucha@users.noreply.github.com>
2024-03-23 13:18:09 +0900

    헤더/푸터 추가

--- front_end/src/pages/Mypage.js
+++ front_end/src/pages/Mypage.js
@@ -1,13 +1,18 @@
-import React from "react";
-import "../styled/pageView.css";
-import MypageSide from "../components/MypageSide";
-import MainPurchaseHistory from "../components/MainPurchaseHistory";
+import React from 'react';
+import '../styled/pageView.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import MypageSide from '../components/MypageSide';
+import MainPurchaseHistory from '../components/MainPurchaseHistory';
 
 function MypageView() {
   return (
+    <>
+    <Header />
+
     <div className="cyj_content_area my-page-content">
       {/* 사이드바 넣는 곳 */}
-      <div className="my-page-sidebar">
+       <div className="my-page-sidebar">
         <MypageSide />
       </div>
       <div className="my_home">
@@ -48,7 +53,7 @@ function MypageView() {
           <div>
             <div className="purchase_list all ask">
               <div className="recent_purchase area">
-                {/* DB로 파일 불러오기 */}
+                  {/* DB로 파일 불러오기 */}
                 <p className="desc">거래 내역이 없습니다</p>
               </div>
             </div>
@@ -56,6 +61,9 @@ function MypageView() {
         </div>
       </div>
     </div>
+    
+    <Footer />
+    </>
   );
 }
 
