e48a275100d063e5dddeb512d835a99c68db0469
유혜원 <yhw9212@naver.com>
2024-03-23 13:30:00 +0900

    회원가입페이지 placeholder 수정

--- front_end/src/pages/SignUpEasy.js
+++ front_end/src/pages/SignUpEasy.js
@@ -1,33 +1,22 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../styled/SignUp.css";
-<<<<<<< HEAD
-// import Header from "./Header";
-// import Footer from "./Footer";
-=======
 import Header from "../components/Header";
 import Footer from "../components/Footer";
->>>>>>> 653a7cd12dcc891b2bd7fbca801f3a95682d62ef
 
 const SignUpEasy = () => {
 
   return (
     <>
-<<<<<<< HEAD
-      {/* <Header /> */}
-=======
       <Header />
->>>>>>> 653a7cd12dcc891b2bd7fbca801f3a95682d62ef
       <div className="yhw_container">
         <form className="yhw_signForm">
           <h4>환영합니다! 정보를 입력해주세요.</h4>
           <div className="yhw_signInputBox">
             <span className="yhw_signInputTit">닉네임</span>
             <input
-              // value={userNumber}
-              // onChange={(e) => setUserNumber(e.target.value)}
               type="text"
-              placeholder="전화번호"
+              placeholder="닉네임"
               required
               />
               <button className="yhw_dupCheckBtn">중복확인</button>
@@ -36,10 +25,7 @@ const SignUpEasy = () => {
             <span className="yhw_signInputTit">나이</span>
             <input
               className="yhw_lastSignInput"
-              // value={userAddress}
-              // onChange={(e) => setUserAddress(e.target.value)}
               type="number"
-              placeholder="나이"
               required
               />
             <span>세</span>
@@ -55,13 +41,9 @@ const SignUpEasy = () => {
             <button type="submit">회원가입</button>
           </div>
         </form>
-      </div>  {/* yhw_container 끝 */}
+      </div>
       
-<<<<<<< HEAD
-      {/* <Footer /> */}
-=======
       <Footer />
->>>>>>> 653a7cd12dcc891b2bd7fbca801f3a95682d62ef
     </>
   );
 };
--- front_end/src/pages/SignUpMail.js
+++ front_end/src/pages/SignUpMail.js
@@ -1,17 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../styled/SignUp.css";
-<<<<<<< HEAD
-// import Header from "./Header";
-// import Footer from "./Footer";
-
-const SignUpMail = () => {
-
-  return (
-    <>
-      {/* <Header /> */}
-      
-=======
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
@@ -80,143 +69,70 @@ const SignUpMail = () => {
   return (
     <>
       <Header />
->>>>>>> 653a7cd12dcc891b2bd7fbca801f3a95682d62ef
       <div className="yhw_container">
         <form className="yhw_signForm">
           <h4>환영합니다! 정보를 입력해주세요.</h4>
           <div className="yhw_signInputBox">
-<<<<<<< HEAD
-            <span className="yhw_signInputTit">이메일</span>
-            <input
-              // value={userEmail}
-              // onChange={(e) => setUserEmail(e.target.value)}
-              type="email"
-              placeholder="이메일 입력"
-              required
-            />
-          </div>
-          <div className="yhw_signInputBox">
-=======
->>>>>>> 653a7cd12dcc891b2bd7fbca801f3a95682d62ef
             <span className="yhw_signInputTit">아이디</span>
             <input
-              // value={userEmail}
-              // onChange={(e) => setUserEmail(e.target.value)}
               type="text"
-<<<<<<< HEAD
-              placeholder="아이디 입력"
-              required
-              />
-              <button className="yhw_dupCheckBtn">중복확인</button>
-=======
               name="userid"
               placeholder="아이디"
               value={userData.userid}
               onChange={handleChange}
             />
             <button className="yhw_dupCheckBtn">중복확인</button>
->>>>>>> 653a7cd12dcc891b2bd7fbca801f3a95682d62ef
           </div>
           <div className="yhw_signInputBox">
             <span className="yhw_signInputTit">비밀번호</span>
             <input
-              // value={userPassword}
-              // onChange={(e) => setUserPassword(e.target.value)}
               type="password"
-<<<<<<< HEAD
-              placeholder="비밀번호"
-              required
-=======
               name="userpwd"
               placeholder="비밀번호"
               value={userData.userpwd}
               onChange={handleChange}
->>>>>>> 653a7cd12dcc891b2bd7fbca801f3a95682d62ef
             />
           </div>
           <div className="yhw_signInputBox">
             <span className="yhw_signInputTit">비밀번호 확인</span>
             <input
-              // value={checkPassword}
-              // onChange={(e) => setCheckPassword(e.target.value)}
               type="password"
               placeholder="비밀번호 확인"
               required
-<<<<<<< HEAD
-              />
-=======
             />
           </div>
           <div className="yhw_signInputBox">
             <span className="yhw_signInputTit">이메일</span>
             <input
-              // value={userEmail}
-              // onChange={(e) => setUserEmail(e.target.value)}
               type="text"
               name="email"
               placeholder="이메일"
               value={userData.email}
               onChange={handleChange}
             />
->>>>>>> 653a7cd12dcc891b2bd7fbca801f3a95682d62ef
           </div>
           <div className="yhw_signInputBox">
             <span className="yhw_signInputTit">닉네임</span>
             <input
-              // value={userNumber}
-              // onChange={(e) => setUserNumber(e.target.value)}
               type="text"
-<<<<<<< HEAD
-              placeholder="전화번호"
-              required
-              />
-              <button className="yhw_dupCheckBtn">중복확인</button>
-=======
-              name="nickname"
               placeholder="닉네임"
               value={userData.nickname}
               onChange={handleChange}
             />
             <button className="yhw_dupCheckBtn">중복확인</button>
->>>>>>> 653a7cd12dcc891b2bd7fbca801f3a95682d62ef
           </div>
-          <div className="yhw_signInputBox">
             <span className="yhw_signInputTit">나이</span>
             <input
               className="yhw_lastSignInput"
-              // value={userAddress}
-              // onChange={(e) => setUserAddress(e.target.value)}
-<<<<<<< HEAD
-              type="number"
-              placeholder="나이"
-              required
-              />
-=======
               type="text"
               name="age"
-              placeholder="나이"
               value={userData.age}
               onChange={handleChange}
             />
->>>>>>> 653a7cd12dcc891b2bd7fbca801f3a95682d62ef
             <span>세</span>
           </div>
           <div className="yhw_signInputBox">
             <span className="yhw_signInputTit">성별</span>
-<<<<<<< HEAD
-              <div className="yhw_signGenderBtns">
-                <button>남자</button>
-                <button>여자</button>
-              </div>
-          </div>
-          <div className="yhw_signUpBtnBox">
-            <button type="submit">회원가입</button>
-          </div>
-        </form>
-      </div>  {/* yhw_container 끝 */}
-      
-      {/* <Footer /> */}
-=======
             <div className="yhw_signGenderBtns">
               <button>남자</button>
               <button>여자</button>
@@ -228,10 +144,8 @@ const SignUpMail = () => {
             </button>
           </div>
         </form>
-      </div>{" "}
-      {/* yhw_container 끝 */}
+      </div>
       <Footer />
->>>>>>> 653a7cd12dcc891b2bd7fbca801f3a95682d62ef
     </>
   );
 };
