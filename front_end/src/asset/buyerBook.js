0c6a39e90177b7bfddb8a398577005ab57d9f889
Bogyoi <86239847+sonlucy@users.noreply.github.com>
2024-03-26 13:08:10 +0900

    구매희망도서정보 데이터

---
+++ front_end/src/asset/buyerBook.js
@@ -0,0 +1,62 @@
+export const buyerBookData = [
+  {
+    "image": 'img/book.png',
+    "itemBuyKey": 1,
+    "custKey": 101,
+    "itemTitle": "책제목1",
+    "author": "책저자",
+    "publisher": "출판",
+    "expiry": 20230301, 
+    "aucStatus": 2
+  },
+  {
+    "image": 'img/book.png',
+    "itemBuyKey": 2,
+    "custKey": 102,
+    "itemTitle": "책제목2",
+    "author": "책저자",
+    "publisher": "출판",
+    "expiry": 20230228,
+    "aucStatus": 1
+  },
+  {
+    "image": 'img/book.png',
+    "itemBuyKey": 3,
+    "custKey": 103,
+    "itemTitle": "책제목3",
+    "author": "책저자",
+    "publisher": "출판",
+    "expiry": 20230315,
+    "aucStatus": 3
+  },
+    {
+    "image": 'img/book.png',
+    "itemBuyKey": 1,
+    "custKey": 101,
+    "itemTitle": "책제목1",
+    "author": "책저자",
+    "publisher": "출판",
+    "expiry": 20230301,
+    "aucStatus": 2
+  },
+  {
+    "image": 'img/book.png',
+    "itemBuyKey": 2,
+    "custKey": 102,
+    "itemTitle": "책제목2",
+    "author": "책저자",
+    "publisher": "출판",
+    "expiry": 20230228,
+    "aucStatus": 1
+  },
+  {
+    "image": 'img/book.png',
+    "itemBuyKey": 3,
+    "custKey": 103,
+    "itemTitle": "책제목3",
+    "author": "책저자",
+    "publisher": "출판",
+    "expiry": 20230315,
+    "aucStatus": 3
+  }
+]
