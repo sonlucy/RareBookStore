0313b2804a3b410b59aa897ccf379da11a7b2a05
Bogyoi <86239847+sonlucy@users.noreply.github.com>
2024-03-26 13:16:19 +0900

    DB 데이터에 따라 변수명 수정

--- front_end/src/components/PurchaseReqListEnd.js
+++ front_end/src/components/PurchaseReqListEnd.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import '../styled/PurchaseReqList.css';
 
-/* const PurchaseReqListEnd = ({ title, info, image }) => {
-  return (
-    <div className="sbk-purchase-req-card">
-      <img className="sbk-book-image" src={image} alt={title} />
-      <div className="sbk-text-container">
-        <h2 className="sbk-book-title">{title}</h2>
-        <h2 className="sbk-book-info">{info}</h2>
-      </div>
-
-    </div>
-  );
-};
-
-export default PurchaseReqListEnd; */
-
 const PurchaseReqListEnd = ({ requests }) => {
   return (
     <div>
       {requests.map((request, index) => (
         <div className="sbk-purchase-req-card" key={index}>
-          <img className="sbk-book-image" src={request.image} alt={request.title} />
+          <img className="sbk-book-image" src={request.image} alt={request.itemTitle} />
           <div className="sbk-text-container">
-            <h2 className="sbk-book-title">{request.title}</h2>
-            <h2 className="sbk-book-info">{request.info}</h2>
+            <h2 className="sbk-book-title">{request.itemTitle}</h2>
+            <h2 className="sbk-book-info">{`${request.author} | ${request.publisher}`}</h2>
 
           </div>
 
