c45ace9c297d154ea75cec75ea0c4512d8e637c6
Bogyoi <86239847+sonlucy@users.noreply.github.com>
2024-03-26 13:07:06 +0900

    진헹중/낙찰/기한만료 클릭 시 해당 리스트 불러오기
    진헹중/낙찰/기한만료 한 페이지에서 처리
    - DB의 aucStatus에 따라 배열에 들어감

---
+++ front_end/src/pages/RegRequest.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import '../styled/BookStateCategory.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import PurchaseReqForm from '../components/PurchaseReqForm';
+import PurchaseReqListOngoing from '../components/PurchaseReqListOngoing';
+import PurchaseReqListEnd from '../components/PurchaseReqListEnd';
+import SalesHistoryList from '../components/SalesHistoryList';
+import MyPageSide from '../components/MypageSide';
+import BookSell from '../components/BookSell';
+import { buyerBookData } from '../assets/buyerBook';
+
+function RegRequest() {
+  const [selectedTab, setSelectedTab] = useState('진행중');
+  const [filteredRequests, setFilteredRequests] = useState([]);
+
+  const handleTabChange = (tab) => {
+    setSelectedTab(tab);
+    filterRequests(tab);
+  };
+
+  const filterRequests = (tab) => {
+    let filtered = [];
+    if (tab === '진행중') {
+      filtered = buyerBookData.filter(request => request.aucStatus === 2);
+    } else if (tab === '낙찰') {
+      filtered = buyerBookData.filter(request => request.aucStatus === 1);
+    } else if (tab === '기한만료') {
+      filtered = buyerBookData.filter(request => request.aucStatus === 3);
+    }
+    setFilteredRequests(filtered);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <div style={{ display: 'flex' }}>
+        <MyPageSide />
+        <div className="sbk-container">
+          <div className='sbk-purchase-request-form-title'>
+            <h1>구매 희망 도서 등록</h1>
+            <hr className='sbk-purchase-request-form-hr' />
+          </div>
+          <PurchaseReqForm />
+          <div className='sbk-purchase-request-form-title'>
+            <h1>구매 희망 도서 신청내역</h1>
+          </div>
+          <BookSell onSelect={handleTabChange} />
+          {selectedTab === '진행중' && <PurchaseReqListOngoing requests={filteredRequests} />}
+          {selectedTab !== '진행중' && <PurchaseReqListEnd requests={filteredRequests} />}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default RegRequest;
