9f4b3d797d7e578010a4bc47c4719584523f8f35
Chongmyung Lee <sam.chongmyung@gmail.com>
2024-03-27 10:16:08 +0900

    환경변수 추가

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <GoogleOAuthProvider clientId="594681299704-d5o8si4lgts8kojoic1f7gktqh4a4sjl.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENTID}>
     <React.StrictMode>
       <BrowserRouter>
         <App />
