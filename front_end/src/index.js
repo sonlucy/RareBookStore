25ecd375001367f05e254c145a6e1002e78a96af
Chongmyung Lee <sam.chongmyung@gmail.com>
2024-03-27 09:37:38 +0900

    로그인_회원기입_수정
    App.js, index.js, package.json 수정

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./styled/index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="594681299704-d5o8si4lgts8kojoic1f7gktqh4a4sjl.apps.googleusercontent.com">
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
