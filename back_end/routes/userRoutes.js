dd6db4e94aa125698d3a586499af25aad6564db5
Chongmyung Lee <sam.chongmyung@gmail.com>
2024-03-29 10:33:54 +0900

    server.js와 admin.js 통합본

--- back_end/routes/userRoutes.js
+++ back_end/routes/userRoutes.js
@@ -6,7 +6,7 @@ const userController = require("../controllers/user"); // 유저 컨트롤러 �
 const businessLogic = require("../models/userDB"); // 비지니스 로직
 const admin = require("../models/adminUserDB"); // 비지니스 로직
 
-// 라우트 설정
+// ============= 회원가입 로그인 라우트 설정 =============//
 router.post("/signup", userController.signup); // 회원가입 부분
 router.post("/loginCheck", businessLogic.loginCheck); // 로그인 부분
 router.post("/adminloginCheck", admin.adminloginCheck); // 어드민로그인 부분
@@ -15,5 +15,8 @@ router.post("/checkUserIdDuplicate", businessLogic.checkUserIdDuplicate); // 아
 router.post("/checkNicknameDuplicate", businessLogic.checkNicknameDuplicate); // 닉네임 중복확인
 router.get("/logout", businessLogic.logout); // 로그아웃 부분
 router.get("/checkSession", businessLogic.checkSession); // 세션체크 부분
+// ============= 회원가입 로그인 라우트 설정 =============//
+
+// ============= CRUD 라우트 설정 ============= //
 
 module.exports = router;
