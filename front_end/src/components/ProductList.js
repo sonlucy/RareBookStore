7d55865c6e218a7402bf34b47e54da190e02f579
Chongmyung Lee <sam.chongmyung@gmail.com>
2024-04-04 15:23:14 +0900

    도서리스트 페이지
    - 구매희망도서(buyerBook 테이블)에서 모든 정보 가져오기.
    - 각 도서의 custKey를 이용해 닉네임 출력
    - 등급별 최저가 항목은 판매하기 페이지 구현 후 개발예정
    - CSS 수정 필요.

--- front_end/src/components/ProductList.js
+++ front_end/src/components/ProductList.js
@@ -1,104 +1,111 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
 
+// ============== Style component ============== //
 const CenteredContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const ProductListContainer = styled.div`
-    width: 1050px;
-    height: 300px;
-    display: flex;
-    flex-direction: row;
+  width: 1050px;
+  height: 300px;
+  display: flex;
+  flex-direction: row;
 `;
 
 const ProductImage = styled.div`
-    width: 177px;
-    height: 268px;
-    background: url('/book.jpg');
-    margin: 0 36px 29px;
+  width: 177px;
+  height: 268px;
+  background: url("/book.jpg");
+  margin: 0 36px 29px;
 `;
 
 const ProductInfoContainer = styled.div`
-    margin: 0px;
-    display: flex;
-    flex-wrap: wrap;
-    width: 170px;
-    height: 300px;
-    text-align: left;
+  margin: 0px;
+  display: flex;
+  flex-wrap: wrap;
+  width: 170px;
+  height: 300px;
+  text-align: left;
 `;
 
 const ProductInfo = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    height: 100px;
-    justify-content: left;
-    align-items: flex-start;
+  display: flex;
+  flex-wrap: wrap;
+  height: 100px;
+  justify-content: left;
+  align-items: flex-start;
 `;
 
 const PriceTable = styled.table`
-    width: 100%;
-    height: 100px;
-    display: flex;
-    flex-wrap: wrap;    
-    justify-content: left;
-    align-items: flex-end;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: left;
+  align-items: flex-end;
 `;
 
 const SellButton = styled.button`
-    width: 160px;
-    height: 50px;
-    border-radius: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    background-color: #C87E66;
-    margin-left: auto;
-    margin-right: 46px;
-    margin-top: 9px;
+  width: 160px;
+  height: 50px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  background-color: #c87e66;
+  margin-left: auto;
+  margin-right: 46px;
+  margin-top: 9px;
 `;
-
+// ============== Style component ============== //
 
 function ProductList(props) {
-    return (
-        <CenteredContainer>
-        <ProductListContainer>
-            <ProductImage></ProductImage>
-            <ProductInfoContainer>
-                <ProductInfo>
-                <span>폴링인 폴</span>
-                <span>모건하우절 | 문학동네</span>
-                <span>아기 돌고래</span>
-                </ProductInfo>
-                <PriceTable>
-                    <thead>
-                        <tr>
-                            <th>등급별최저가</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>최상</td>
-                            <td>12000</td>
-                        </tr>
-                        <tr>
-                            <td>상</td>
-                            <td>12000</td>
-                        </tr>
-                        <tr>
-                            <td>중</td>
-                            <td>12000</td>
-                        </tr>
-                    </tbody>
-                </PriceTable>
-            </ProductInfoContainer>
-            <SellButton>판매하기</SellButton>
-        </ProductListContainer>
-        </CenteredContainer>
-    );
+  return (
+    <CenteredContainer>
+      <ProductListContainer>
+        <ProductImage></ProductImage>
+        <ProductInfoContainer>
+          <ProductInfo>
+            <span>{props.bookList.itemTitle}</span>
+            <br />
+            <span>
+              {props.bookList.author} | {props.bookList.publisher}
+            </span>
+
+            <span>{props.bookList.nickname}</span>
+          </ProductInfo>
+
+          <PriceTable>
+            <thead>
+              <tr>
+                <th>등급별최저가</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>최상</td>
+                <td>12000</td>
+              </tr>
+              <tr>
+                <td>상</td>
+                <td>12000</td>
+              </tr>
+              <tr>
+                <td>중</td>
+                <td>12000</td>
+              </tr>
+            </tbody>
+          </PriceTable>
+        </ProductInfoContainer>
+        <SellButton>판매하기</SellButton>
+      </ProductListContainer>
+    </CenteredContainer>
+  );
 }
 
 export default ProductList;
