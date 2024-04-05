dc5139efba7f1b057bbe1136fc421a2dda1dc4aa
유혜원 <yhw9212@naver.com>
2024-04-05 14:39:57 +0900

    DetailConts.js - useState 추가

--- front_end/src/components/DetailConts.js
+++ front_end/src/components/DetailConts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styled/DetailConts.css";
 import SellerRanking from "../components/SellerRanking";
