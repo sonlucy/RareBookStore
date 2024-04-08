57c595ca1bd3c719fd5b8de38bbae4c8deec8165
Chongmyung Lee <sam.chongmyung@gmail.com>
2024-04-08 17:38:26 +0900

    현황보기 db연결
    - 마이페이지/구매 희망 도서/구매 희망 도서 신청내역에서 현황보기 클릭시 BuyDetail페이지로 넘어가도록 연결.
    
    - 책 정보 및 상태 정보 db연결
    
    - 추후에 판매자 번호가 아닌 닉네임으로 수정 및 구매하기 링크 연결 필요.

--- front_end/src/components/DetailTop.js
+++ front_end/src/components/DetailTop.js
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styled/DetailTop.css";
 
-const DetailTop = () => {
+const DetailTop = ({ bookInfo }) => {
   return (
-    <div className="yhw_detailTopProdBox">  {/* 이미지, 도서명, 저자, 판매자, 판매 입찰 버튼 감싸는 div */}
+    <div className="yhw_detailTopProdBox">
+      {" "}
+      {/* 이미지, 도서명, 저자, 판매자, 판매 입찰 버튼 감싸는 div */}
       <div className="yhw_detailTopProdInfoBox">
-        <img src="https://via.placeholder.com/180x240" alt="상품이미지" />
+        <img src={bookInfo.itemImg} alt="상품이미지" />
         <div className="yhw_detailTopProdInfoTxt">
           <div className="yhw_detailTopProdInfoTop">
-            <b>{/* 도서명 받아올 부분 */}도서명</b>
-            <span>{/* 저자 받아올 부분 */}저자</span>
-            <span className="yhw_detailTopBuyer">구매자 {/* 구매자 받아올 부분 */}아기돌고래</span>
+            <b>{bookInfo.itemTitle}도서명</b>
+            <span>{bookInfo.author}저자</span>
+            {/* <span className="yhw_detailTopBuyer">구매자 {loginUser}</span> */}
           </div>
           <div className="yhw_detailTopProdInfoBottom">
             <span className="yhw_detailTopDeadline">입찰 마감 기한</span>
-            <span className="yhw_detailTopDate">{/* 마감 날짜 받아올 부분 */}2024-03-19</span>
+            <span className="yhw_detailTopDate">{bookInfo.expiry}</span>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default DetailTop;
--- front_end/src/pages/BuyDetail.js
+++ front_end/src/pages/BuyDetail.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { VscChevronRight } from "react-icons/vsc";
 import "../styled/BuyDetail.css";
 import Header from "../components/Header";
@@ -9,73 +9,139 @@ import DropDownSort from "../components/DropDownSort";
 import DetailConts from "../components/DetailConts";
 import Footer from "../components/Footer";
 import { productInfosData } from "../asset/productInfosData";
+import axios from "axios";
 
 const BuyDetail = () => {
-  const [selectedState, setSelectedState] = useState('최상'); // 초기 선택된 상태 설정
+  const [selectedState, setSelectedState] = useState("최상"); // 초기 선택된 상태 설정
   const [filteredProducts, setFilteredProducts] = useState([]); // 필터링된 상품 정보 상태 ==> 초깃값으로는 아무것도 없는 빈 배열을 설정
+  const { itemBuyKey } = useParams();
+  const [bookInfo, setBookInfo] = useState([]);
+  const [sellerInfo, setSellerInfo] = useState([]);
+  const [sellerNickname, setSellerNickname] = useState([]);
+  // 특정 구매희망 도서 가져오기
+  const getBookInfo = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/buyerbook/item/${itemBuyKey}`
+      );
+      setBookInfo(response.data[0]);
+    } catch (error) {
+      console.error("Error fetching customers:", error);
+    }
+  };
+  // 특정 도서 판매자 정보 가져오기
+  const getSeller = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/sellerbook/item/${itemBuyKey}`
+      );
+
+      setSellerInfo(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  // const getSellerNickname = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       `http://localhost:3001//customers/${sellerInfo.sellerKey}`
+  //     );
+  //     console.log(response.data);
+  //     setSellerNickname(response.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
   // 초기 렌더링 시 최상 상태에 해당하는 상품 필터링
   useEffect(() => {
-    filterProductsByState('최상');
-    console.log(filterProductsByState);
+    getBookInfo();
+    getSeller();
   }, []);
+  useEffect(() => {
+    handleStateChange("최상");
+  }, [sellerInfo]);
 
   // StateCategory에서 선택된 상태를 처리하는 함수
   const handleStateChange = (state) => {
     setSelectedState(state); // 선택된 상태 업데이트
     filterProductsByState(state); // 선택된 상태에 따라 상품 필터링
   };
-  
+
   // 선택된 상태에 따라 상품 필터링 함수
   const filterProductsByState = (selectedStt) => {
     let filtered;
     // 선택된 상태에 따라 상품 필터링
-    if (selectedStt === '최상') {
-      filtered = productInfosData.filter(product => product.condition.length === 0);
-    } else if (selectedStt === '상') {
-      filtered = productInfosData.filter(product => product.condition.length === 1);
-    } else if (selectedStt === '중') {
-      filtered = productInfosData.filter(product => product.condition.length >= 2);
+    if (selectedStt === "최상") {
+      filtered = sellerInfo.filter((product) => product.damage === 0);
+    } else if (selectedStt === "상") {
+      filtered = sellerInfo.filter((product) => product.damage === 1);
+    } else if (selectedStt === "중") {
+      filtered = sellerInfo.filter((product) => product.damage >= 2);
     } else {
-      console.log('상태 선택 오류');
+      console.log("상태 선택 오류");
     }
     setFilteredProducts(filtered); // 필터링된 상품 업데이트
+    console.log(filtered, "필터된 데이터");
   };
-  
+
   return (
     <>
       <div className="height-container">
-      <Header />
+        <Header />
 
-      <div className="yhw_container">
-        <div className="yhw_detailCont">  {/* Top부분 전체 감싸는 div */}
-          <div className="yhw_detailTopBox">
-            <div className="yhw_detailTopNav">  {/* Top부분 - (절판서점>카테고리>책제목) 감싸는 div */}
-              <Link to="/"><span>절판서점</span></Link>
-              <span><VscChevronRight /></span>
-              <Link to="/"><span>{/* 카테고리 받아올 부분 */}카테고리</span></Link>
-              <span><VscChevronRight /></span>
-              <Link to="/"><span>{/* 책제목 받아올 부분 */}책제목</span></Link>
+        <div className="yhw_container">
+          <div className="yhw_detailCont">
+            {" "}
+            {/* Top부분 전체 감싸는 div */}
+            <div className="yhw_detailTopBox">
+              <div className="yhw_detailTopNav">
+                {" "}
+                {/* Top부분 - (절판서점>카테고리>책제목) 감싸는 div */}
+                <Link to="/">
+                  <span>절판서점</span>
+                </Link>
+                <span>
+                  <VscChevronRight />
+                </span>
+                <Link to="/">
+                  <span>{bookInfo.category}</span>
+                </Link>
+                <span>
+                  <VscChevronRight />
+                </span>
+                <Link to="/">
+                  <span>{bookInfo.itemTitle}책제목</span>
+                </Link>
+              </div>
+              <DetailTop bookInfo={bookInfo} />
             </div>
-            <DetailTop />
-          </div>
-          <br />
-          <div className="yhw_deailStatCatBox">
-            <StateCategory onStateChange={handleStateChange} />
-          </div>
-          <div className="yhw_detailMainBox">
-            <DropDownSort />
-            {/* 필터링된 상품 정보를 기반으로 상품 내용을 화면에 표시 */}
-            {filteredProducts.map((productInfo, index) => (
-              <DetailConts key={index} productInfo={productInfo} />
-            ))}
-          </div>
-        </div>  {/* yhw_detailCont 끝 */}
-      </div>
+            <br />
+            <div className="yhw_deailStatCatBox">
+              <StateCategory
+                onStateChange={handleStateChange}
+                filteredProducts={filteredProducts}
+              />
+            </div>
+            <div className="yhw_detailMainBox">
+              <DropDownSort />
+              {/* 필터링된 상품 정보를 기반으로 상품 내용을 화면에 표시 */}
+              {filteredProducts.map((productInfo, index) => (
+                <DetailConts
+                  key={index}
+                  productInfo={productInfo}
+                  bookInfo={bookInfo}
+                  sellerInfo={sellerInfo}
+                />
+              ))}
+            </div>
+          </div>{" "}
+          {/* yhw_detailCont 끝 */}
+        </div>
       </div>
       <Footer />
     </>
   );
-}
+};
 
 export default BuyDetail;
